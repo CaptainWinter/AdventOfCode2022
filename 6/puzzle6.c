8d4cd7ef049cea4e295bf3e05d886a1bb438dc27
Captain Winter <manueljoao2000@gmail.com>
2022-12-06 18:38:05 +0000

    Day 6
    optimized code readability

--- 6/puzzle6.c
+++ 6/puzzle6.c
@@ -1,18 +1,43 @@
-// 1892
-// sqzgdbflhvpwcr
-// 2313
-
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+int checkChars(int count, char* string, int size)
+{
+    // char j to char j+2
+    for (int j = 0; j < size-1; j++)
+    {
+        // from char j to char j+3
+        for (int k = j+1; k < size; k++)
+        {
+            if (string[count+j] == string[count+k])
+                return 1;
+        } 
+    }
+    return 0;
+}
+
+int counting(char* string, int size)
+{
+    int count = 0, flag = 0;
+    // Entire line
+    for (int i = 0; i <= (strlen(string)); i++)
+    {
+        flag  = checkChars(count, string, size);
+        count += flag;
+        if (flag == 0)
+            return count + size;
+    }
+    return 0;
+}
 
 int main()
 {
     ssize_t read;
     size_t len = 0;
     char* string;
-    int count = 0, flag = 0, part = 2;
+    int count = 0, part = 2;
     FILE *f = fopen("input.txt", "r");
 
     if (f == NULL)
@@ -23,59 +48,14 @@ int main()
 
     while ((read = getline(&string, &len, f)) != -1)
     {
+        // the chars of the solution wont trigger the funtion
         if (part ==1)
-        {
-            // Entire line
-            for (int i = 0; i <= (strlen(string)); i++)
-            {
-                flag  = 0;
-                // char j to char j+2
-                for (int j = 0; j < 3; j++)
-                {
-                    // from char j to char j+3
-                    for (int k = j+1; k < 4; k++)
-                    {
-                        if (string[count+j] == string[count+k])
-                            flag = 1;
-                    } 
-                }
-                count += flag;
-                if (flag == 0)
-                    break;
-            }
-        }
+            printf("%i", counting(string, 4));
         else
-        {
-            // Entire line
-            for (int i = 0; i <= (strlen(string)); i++)
-            {
-                flag  = 0;
-                // char j to char j+2
-                for (int j = 0; j < 13; j++)
-                {
-                    // from char j to char j+3
-                    for (int k = j+1; k < 14; k++)
-                    {
-                        if (string[count+j] == string[count+k])
-                            flag = 1;
-                    } 
-                }
-                count += flag;
-                if (flag == 0)
-                    break;
-            }
-        }
-        
-        
+            printf("%i", counting(string, 14));
+            
     }
-    
-    // the chars of the solution wont trigger the funtion
-    if (part == 1)
-        count += 4;
-    else
-        count += 14;
-    
-    printf("%i", count);
+
     fclose(f);
     return 0;
 }
